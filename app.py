02833915947238a9767ec7a5832000d86663efe9
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-03-05 18:39:17 +0530

    Fixed Streamlit startup issue

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import model
+
 import streamlit as st
-from model import predict_trending_topic, df, get_contexts_for_country
+import model
 
 # Streamlit UI
 st.title("Twitter Trending Topics Predictor")
 
-country = st.selectbox("Select Country", df['country'].unique())
-context_options = get_contexts_for_country(country)
+country = st.selectbox("Select Country", model.df['country'].unique())
+context_options = model.get_contexts_for_country(country)
 context = st.selectbox("Select Context", context_options)
 
 if st.button("Predict Trending Topic"):
-    trending_topic = predict_trending_topic(country, context)
+    trending_topic = model.predict_trending_topic(country, context)
     st.success(f"Predicted Trending Topic: {trending_topic}")
